658cd1869c0278f7f418f25a01da0231e30df4d6
Vladimir Panteleev <v.panteleev@paralect.com>
2020-06-07 20:57:25 +0300

    init

--- index.js
+++ index.js
@@ -2,43 +2,31 @@ let levenshtein = require('fast-levenshtein');
 let carbranns = require('./car-models.json');
 
 
+let example = "Mustang";
 
-function extendWithDistance(arrayOfObjects, propName, referenceString) {
-    return arrayOfObjects.map((object) => {
-        let distance = levenshtein.get(object[propName], referenceString);
-        
-        return Object.assign(object, {distance});
-    })
-}
-
-function findMinimalDistance(arrayOfObjects) {
-    return  arrayOfObjects.reduce((min, p) => p.distance < min ? p.distance : min, arrayOfObjects[0].distance);
-} 
-
-
-
-let example = "Fort Edje";
-
- let manufacturersWithDistance = extendWithDistance(carbranns, "brand", example);
-
- let minDistanceManufacturer = findMinimalDistance(manufacturersWithDistance);
-
-let bestMatchManufacturer = manufacturersWithDistance.filter((model) => model.distance === minDistanceManufacturer)[0]; // here we cann add parametr for getting "close to minimum" results
-
-let modelsWithDistance = bestMatchManufacturer.models.map(modelName => {
+let calculatedBrands = carbranns.map(manufacturer => {
+    let models =  manufacturer.models.map(modelName => {
         let distance =  levenshtein.get(modelName, example);
         return {modelName, distance};
     })
 
-bestMatchManufacturer.models = modelsWithDistance;
+    return {...manufacturer, models}
+})
 
-let minDistanceModel = findMinimalDistance(bestMatchManufacturer.models);
 
-let bestMatchModel = bestMatchManufacturer.models.filter((model) => model.distance === minDistanceModel)[0]; // here we cann add parametr for getting "close to minimum" results
+let min = calculatedBrands[0].models[0].distance;
+let brandName = calculatedBrands[0].brand;
+
+calculatedBrands.forEach(manufacturer => {
+    manufacturer.models.forEach(model => {
+        if (model.distance < min) {
+            min = model.distance;
+            brandName = manufacturer.brand;
+        }
+    })
+})
+
+console.log(`Reference string is [${example}]`);
 
 console.log("Best match manufacturer with models list: \n")
-console.log(bestMatchManufacturer);
-
-console.log("\nBest match model: \n")
-console.log(bestMatchModel.modelName);
-
+console.log(brandName);
